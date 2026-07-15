ca729d76d3d9bb347e30a9fd261777fea5515d23
NepGliding <18307587985@163.com>
2026-07-15 22:41:42 +0800

    First filing 260715 2241

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/vwhat/',
   title: 'What can I saw ？',
   description: 'A VitePress Site',
   themeConfig: {
