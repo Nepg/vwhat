f106bed0d9a0270f5dbf243aea201e05ef2c0938
NepGliding <18307587985@163.com>
2026-07-30 20:33:31 +0800

    72th filing 260730 2033

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -18,7 +18,8 @@ export default defineConfig({
         items: [
           { text: '一些资源（win）', link: '/resources_win' },
           { text: '一些资源（安卓）', link: '/resources_android' },
-          { text: '方舟服务器常用', link: '/ARK_Mod_Cmd' },
+          { text: 'ARK服务端常用', link: '/ARK_Mods_Cmd' },
+          { text: 'ARK模组清理', link: '/ARK_Mods_Clean' },
           {
             text: '制作 UE4 数值修改模组（HumanitZ）',
             link: '/UE4_Mod_ValueDev',
